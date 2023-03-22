ac26434042c55dccf44c36844d172cd75fa1ee8e
HosneMobarak262 <hosne.1504@gmail.com>
2023-03-23 00:56:38 +0600

    assignment one

---
+++ assignment-1.js
@@ -0,0 +1,10 @@
+function multiplicationTable(num) {
+    for(var i = 1; i <=10; i++){
+        console.log(num + " * " + i + " = " + num * i);
+    }
+}
+
+multiplicationTable(12);
+multiplicationTable(1145);
+multiplicationTable(1545);
+multiplicationTable(125);
