8a7d5fc8215bb254245e98c8ae137f098c1ab293
HosneMobarak262 <hosne.1504@gmail.com>
2023-06-10 00:38:54 +0600

    refactor : jwt token generate using symmetric key

---
+++ assignment-10/src/controllers/JwtTokenController.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+exports.generateToken = (req, res) => {
+   try{
+       const {userId, password} = req.body;
+
+       if(!userId){
+           return res.json({message: "Please enter userId"});
+       }
+       if(!password || password.length < 8 ){
+           return res.json({message: "Please enter password with minimum length of 8."});
+       }
+
+       const payload = { userId: userId, password: password };
+       const secretKey = process.env.JWT_SECRET_KEY;
+       const expiresIn = '6h';
+
+   //    generate token
+       const generateToken = (payload, secretKey, expiresIn) => {
+           return jwt.sign(payload, secretKey, {expiresIn});
+       };
+
+       const token = generateToken(payload, secretKey, expiresIn);
+
+       res.status(200).json({token: token});
+
+   } catch (err) {
+       res.status(500).json({message: "Internal Server Error. Could not generate token.", error: err});
+       console.log(err);
+   }
+};
--- assignment-10/src/routes/routes.js
+++ assignment-10/src/routes/routes.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const {createProduct, getAllProducts} = require('../controllers/ProductController');
+const {generateToken} = require('../controllers/JwtTokenController');
 
 router.post("/product", createProduct);
 router.get("/product", getAllProducts);
+router.get("/gettoken", generateToken)
 
 module.exports = router;
