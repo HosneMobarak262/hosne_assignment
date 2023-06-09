ec14134415b97c9ad8f8e58d08c9455da230dce4
HosneMobarak262 <hosne.1504@gmail.com>
2023-06-10 00:01:46 +0600

    fix : assignment 10 route fix

--- assignment-10/src/routes/routes.js
+++ assignment-10/src/routes/routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const {createProduct, getAllProducts} = require('../controllers/ProductController');
 
-router.post("./product", createProduct);
-router.get("./product", getAllProducts);
+router.post("/product", createProduct);
+router.get("/product", getAllProducts);
 
 module.exports = router;
