1beb3827b420029bb06ee72d9773e5694ea9315c
HosneMobarak262 <hosne.1504@gmail.com>
2023-08-17 21:20:19 +0600

    assignment-18 : server route create

--- assignment-18-blog-crud/src/route/api.js
+++ assignment-18-blog-crud/src/route/api.js
@@ -1,8 +1,15 @@
 const express=require('express');
 const router=express.Router();
 
-// API Routing End Point
+const BlogController = require("../controller/BlogController")
+const {getAllBlogs} = require("../controller/BlogController");
 
+// API Routing End Point
+router.get("/blog", BlogController.getAllBlogs);
+router.get("/blog/:id", BlogController.getBlogByID);
+router.post("/blog", BlogController.createBlog);
+router.put("/blog/:id", BlogController.updateBlogByID);
+router.delete("/blog/:id", BlogController.deleteBlogByID);
 
 
 module.exports=router;
